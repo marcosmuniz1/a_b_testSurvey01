961179994766e6e55d67a827a53a8d35e0194787
Marcos Muñiz <marcosmuniz93@gmail.com>
2023-10-13 20:29:30 +0200

    added duration histograms
    added a chart at the start to see the distribution of duration across variants, imported pyplot

--- survey_abtest.py
+++ survey_abtest.py
@@ -6,6 +6,19 @@ from scipy.stats import ttest_ind
 os.chdir("C:/Users/marco/Dropbox (MPD)/Analytics Argentina/Personal Files/MM/Python/PYMNTS_Stuff/user_cluster_analysis")
 df=pd.read_csv("survey_data.csv")
 
+# No.1 is survey duration so lets start by looking at that
+
+duration_a = df[df['variant'] == 'A']['completion_minutes']
+duration_b = df[df['variant'] == 'B']['completion_minutes']
+
+plt.hist(duration_a, histtype='stepfilled', density=True, color="lightblue",label='Variant A')
+plt.hist(duration_b, histtype='step',density=True, label='Variant B')
+plt.xlabel('Duration (minutes)')
+plt.ylabel('Frequency')
+plt.legend()
+plt.title('Frequency Distribution of Survey Duration')
+plt.savefig('histogram.png')
+
 print(df.groupby('variant')['completion_minutes'].mean())
 
 # These look somewhat different. let's test for equality of means
@@ -16,15 +29,12 @@ print(df.groupby('variant')['completion_minutes'].mean())
 ## statistically significant difference between the means of two independent groups (samples). 
 # the only prerequiste pending is to assess equality of variances, i can use Levene's test
 
-duration_a = df[df['variant'] == 'A']['completion_minutes']
-duration_b = df[df['variant'] == 'B']['completion_minutes']
-
 stat, p_value = levene(duration_a,duration_b)
 
 if p_value < 0.05:
-    print("The variances are significantly different (heteroscedastic).")
+    print("The variances are significantly different (heteroscedastic)")
 else:
-    print("There is no significant difference in variances (homoscedastic).")
+    print("There is no significant difference in variances (homoscedastic)")
 
 # now we can do student's test
 
