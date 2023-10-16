4c098a470de716ed59c47eb30dad686f636463e5
Marcos Muñiz <marcosmuniz93@gmail.com>
2023-10-16 21:56:27 +0200

    added pending proportion tests
    testing for proportions in questions b,c, d e via looping. Also added a counter bc even as all test H0 fails to be rejected it may not be the case when swapping samples for a new test so it may serve as template for further testings

--- survey_abtest.py
+++ survey_abtest.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-# Define a custom color palette of greens
+# plus, defining a custom color palette of greens
 green_palette3 = sns.color_palette("Greens", n_colors=3)
 green_palette4 = sns.color_palette("Greens", n_colors=4)
 green_palette5 = sns.color_palette("Greens", n_colors=5)
@@ -20,7 +20,10 @@ print("\n**********\n")
 # Problem statement ###################
 
 print("Variant column indicates whether response comes from which variant in the A/B test")
-print('Goal is to show wether there are significant differences in survey proportions for affected questions and wether time or bad respondent rate gets worse as a result of the change\n')
+print('Goal is to show whether there are significant differences in survey proportions for affected questions and wether time or bad respondent rate gets worse as a result of the change\n')
+print('I will establish to counters, testnum and fails and we can later make a decision based on the number of test failed (i.e detectable changes)')
+testnum=0
+fails=0
 print('Easiest seems to be survey duration so lets start by looking at that')
 print("\n**********\n")
 
@@ -47,7 +50,7 @@ print("\n**********\n")
 print('These look somewhat different. let"''"s test for equality of means')
 print("i wll be using student's test, so first going to use levine's to test variance similarity\n")
 
-# levene
+# levene test
 
 print ("Levene's test for variance differences:")
 stat, p_value = levene(duration_a,duration_b)
@@ -67,14 +70,18 @@ print(f'P-value: {p_value}')
 
 if p_value < 0.05:
     print("The difference in mean duration is statistically significant.\n")
+    testnum+=1
+    fails+= 1
 else:
     print("There is no statistically significant difference in mean duration.\n")
+    testnum+=1
+    fails+= 0
     
 print('on to proportion differences')
 print("\n**********\n")
 
 
-# Step 2: survey response proportions: question A #####
+# Step 2: survey response proportions: question A ###################
 
 print("question A:")
 
@@ -103,7 +110,7 @@ for p in ax.patches:
 plt.tight_layout()
 #plt.savefig('a_counts_compared.png')
 
-print("makes more sense to use a single metric to test, so im going to merge answers assoicated with significant interest and test wether they vary by variant ")
+print("makes more sense to use a single metric to test, so im going to merge answers associated with significant interest and test wether they vary by variant ")
 print("checking for proportion differences in response rates for Somewhat interested or more:")
 
 # reseting question a) so it only has two values:
@@ -137,8 +144,12 @@ print(f"P-value: {pval}")
 alpha = 0.05  # Set your desired significance level
 if pval < alpha:
     print("There is a significant difference between variants A and B for question a.")
+    testnum+=1
+    fails+= 1
 else:
     print("There is no significant difference between variants A and B for question a.")
+    testnum+=1
+    fails+= 0
 
 print("\n**********\n")
     
@@ -169,5 +180,78 @@ print(f"P-value: {pval}")
 alpha = 0.05  # Set your desired significance level
 if pval < alpha:
     print("There is a significant difference between variants A and B in terms of BR rates.")
+    testnum+=1
+    fails+= 1
 else:
     print("There is no significant difference between variants A and B in terms of BR rates.")
+    testnum+=1
+    fails+=0
+    
+# Step 4: Pending response rates ##########################
+
+print("remaining proportions to evaluate:\n")
+for q in list([df.answer_b,df.answer_c,df.answer_d,df.answer_e]):
+    print(q.value_counts(normalize=True))
+    print("*"*3)
+    
+# b, c and d are analogue so i can probably loop
+
+for q in list(['answer_b','answer_c','answer_d']):
+    print(f"sample proportion equality testing on {q}")
+    count_A = len(variant_A[variant_A[q] == "Yes"])
+    count_B = len(variant_B[variant_B[q] == "Yes"])
+
+    n_A = len(variant_A)
+    n_B = len(variant_B)
+
+    # Perform the two-proportion z-test
+    stat, pval = proportions_ztest([count_A, count_B], [n_A, n_B])
+
+    # Output the test results
+    print(f"Z-statistic: {stat}")
+    print(f"P-value: {pval}")
+
+    # Determine significance
+    alpha = 0.05  # Set your desired significance level
+    if pval < alpha:
+        print(f"There is a significant difference between variants A and B regarding {q}")
+        testnum+=1
+        fails+= 1
+    else:
+        print(f"There is no significant difference between variants A and B regarding {q}")
+        testnum+=1
+        fails+= 0
+    print("****\n")
+    
+print("the process for question e is quite the same, just the loop parameter changing\n")
+
+for u in df.answer_e.unique():
+    print(f"sample proportion equality testing on {u} response rates for question e")
+    count_A = len(variant_A[variant_A['answer_e'] == u])
+    count_B = len(variant_B[variant_B['answer_e'] == u])
+
+    n_A = len(variant_A)
+    n_B = len(variant_B)
+
+    # Perform the two-proportion z-test
+    stat, pval = proportions_ztest([count_A, count_B], [n_A, n_B])
+
+    # Output the test results
+    print(f"Z-statistic: {stat}")
+    print(f"P-value: {pval}")
+
+    # Determine significance
+    alpha = 0.05  # Set your desired significance level
+    if pval < alpha:
+        print(f"There is a significant difference between variants A and B regarding {u} in answer_e")
+        testnum+=1
+        fails+= 1
+    else:
+        print(f"There is no significant difference between variants A and B regarding {u} in answer_e")
+        testnum+=1
+        fails+= 0
+    print("****\n")
+    
+print("CONCLUSION:\n")
+
+print(f"out of {testnum} tests, {fails} indicate significant differences in the results of the alternative designs")
